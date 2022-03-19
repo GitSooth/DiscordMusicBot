5beb4d7cfadadfccd89b5e4f9d7cd13264488cb6
GitSooth <a15823@alunos.ipca.pt>
2022-03-19 19:43:19 +0000

    index js rect

--- index.js
+++ index.js
@@ -31,44 +31,47 @@ for (const file of slashFiles) {
     client.slashCommands.set(slashcmd.data.name, slashcmd);
     if (LOAD_SLASH) 
         commands.push(slashcmd.data.toJSON());
+    
 
 
-    }
+}
 
-    if (LOAD_SLASH) {
-        const rest = new REST({version: "9"}).setToken(TOKEN);
-        console.log("Deploying slash commands");
-        rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {body: commands}).then(() => {
-            console.log("Successfuly loaded!");
-            process.exit(0);
-        }).catch((err) => {
-            if (err) {
-                console.log(err);
-                process.exit(1);
-            }
-        });
-    } else {
-        client.on("ready", () => {
-            console.log(`Logged in as ${
-                client.user.tag
-            }`)
-        });
-        client.on("interactionCreate", (interaction) => {
-            async function handleCommand() {
-                    if (!interaction.isCommand()) 
-                        return;
-
-
-                        const slashcmd = client.slashCommands.get(interaction.commandName);
-                        if (! slashcmd) 
-                            interaction.replied("Not a valid slash command");
+if (LOAD_SLASH) {
+    const rest = new REST({version: "9"}).setToken(TOKEN);
+    console.log("Deploying slash commands");
+    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {body: commands}).then(() => {
+        console.log("Successfuly loaded!");
+        process.exit(0);
+    }).catch((err) => {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+    });
+} else {
+    client.on("ready", () => {
+        console.log(`Logged in as ${
+            client.user.tag
+        }`)
+    });
+    client.on("interactionCreate", (interaction) => {
+        async function handleCommand() {
+            if (!interaction.isCommand()) 
+                return;
+            
 
 
-                            await interaction.deferReply();
-                            await slashcmd.run({client, interaction});
-                        }
-                        handleCommand()
-                    }
-                )
-            };
-            client.login(TOKEN)
+            const slashcmd = client.slashCommands.get(interaction.commandName);
+            if (! slashcmd) 
+                interaction.replied("Not a valid slash command");
+            
+
+
+            await interaction.deferReply();
+            await slashcmd.run({client, interaction});
+        }
+        handleCommand()
+    })
+};
+
+client.login(process.env.TOKEN)
